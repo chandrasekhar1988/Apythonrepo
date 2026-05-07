d5f582feca33ef0fbd61719c9fd48bde961f87f4
chandrasekhar1988 <chadrasekhar.maroju@gmail.com>
2026-05-07 12:09:07 +0530

    Python scripts

--- variables1.py
+++ variables1.py
@@ -1,7 +1,16 @@
+# వేరియబుల్స్ నిర్వచనం
 x = 100
 y = 10.5
 z = "Hello"
 
-print(type(x))  # ఇది 'int' అని చెబుతుంది
-print(type(y))  # ఇది 'float' అని చెబుతుంది
-print(type(z))  # ఇది 'str' అని చెబుతుంది
+# టేబుల్ హెడర్ (Table Header)
+print("-" * 40)
+print(f"{'Variable':<12} | {'Value':<12} | {'Data Type':<10}")
+print("-" * 40)
+
+# f-strings ఉపయోగించి అమరిక
+print(f"{'x':<12} | {x:<12} | {type(x).__name__}")
+print(f"{'y':<12} | {y:<12} | {type(y).__name__}")
+print(f"{'z':<12} | {z:<12} | {type(z).__name__}")
+
+print("-" * 40)
