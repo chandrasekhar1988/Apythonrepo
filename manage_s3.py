a175d95374b6a66f2d5c31c1c778eb0dc93cd554
chandrasekhar1988 <chadrasekhar.maroju@gmail.com>
2026-05-07 17:19:49 +0530

    Python scripts

--- manage_s3.py
+++ manage_s3.py
@@ -1,17 +1,17 @@
 import boto3
 
 s3 = boto3.resource('s3', region_name='us-east-1')
-bucket_name = "devops-chandra-bucket-2026"
+bucket_name = "devops-chandra-bucketA-2026"
 
 try:
     # us-east-1 కోసం CreateBucketConfiguration అవసరం లేదు
     s3.create_bucket(Bucket=bucket_name)
-    print(f"బకెట్ '{bucket_name}' క్రియేట్ చేయబడింది.")
+    print(f"Bucket '{bucket_name}' has been created.")
 except Exception as e:
     print(f"Error: {e}")
 
 # 2. మీ అకౌంట్‌లో ఉన్న అన్ని బకెట్ల పేర్లు చూడటం
-print("\nప్రస్తుతం ఉన్న బకెట్ల జాబితా:")
+print("\nList of current buckets:")
 for bucket in s3.buckets.all():
     print(f"- {bucket.name}")
 
