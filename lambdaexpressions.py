8579b6a85153fcabd4d50027ad4f53262d4ca373
chandrasekhar1988 <chadrasekhar.maroju@gmail.com>
2026-05-07 12:29:02 +0530

    Python scripts

--- lambdaexpressions.py
+++ lambdaexpressions.py
@@ -3,4 +3,11 @@ def add(x, y):
     return x + y
 
 #దీన్నే లాంబ్డాలో ఇలా రాస్తాం:
-add = lambda x, y : x + y
+lambda_add = lambda x, y : x + y
+
+result1 = add(5, 3)
+print(f"Normal Function Result: {result1}")
+
+# Calling the lambda function
+result2 = lambda_add(7, 8)
+print(f"Lambda Function Result: {result2}")
